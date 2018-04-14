5ae9c0a905aed740e94cbc4a3d0e68b311ccdebc
Ebtihal-Saleh <gifted.e.111@gmail.com>
2018-04-14 22:48:06 +0300

    Finish the basic random quotes generator

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
-var twitterlink = document.getElementById(tweetlink);
+var twitterlink = document.getElementById("tweetlink");
 var thequoteplace = document.getElementById("quoteplace");
 var buttn = document.getElementById("butn");
+//var tweetbutn = document.getElementById("tweetlink");
 var clickscounter =0;
 buttn.addEventListener("click", function(){
   var ajaxhttp = new XMLHttpRequest();
@@ -9,9 +10,19 @@ ajaxhttp.onload = function(){
   var mydata = JSON.parse(ajaxhttp.responseText);
   if(clickscounter<mydata.length){
   displaydata(mydata,clickscounter);
-   clickscounter++;} else{clickscounter=0;}
+  /*tweetbutn.addEventListener("click", function(){
+    var url = "https://twitter.com/intent/tweet?text=";
+    var quotetext = mydata[clickscounter].quote+" -"+mydata[clickscounter].author;
+     window.open(url+quotetext);
+  });*/
+  var text = mydata[clickscounter].quote+" -"+mydata[clickscounter].author;
+  var url = "https://twitter.com/intent/tweet/?text="+text;
+  twitterlink.setAttribute("href",url);
+ clickscounter++;} else{clickscounter=0;}
+
 
 };
+
 ajaxhttp.send();
 
 });
@@ -19,6 +30,6 @@ function displaydata(data,n){
 
   var mystring = "<p>"+data[n].quote +"</br> -"+data[n].author+"</p>";
 thequoteplace.innerHTML = mystring;
-twitterlink.setAttribute("href","https://twitter.com/intent/tweet/?text="+data[n].quote);
+
 
 }
